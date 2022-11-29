8e64bfbec8b7faed71ff35dc753e533763c107c5
Rob Duarte <me@robduarte.com>
2022-11-29 11:47:42 -0500

    done

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+var drawText = false
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(100, 100)
+  let btn = createButton("Show/Hide Text")
+  btn.mousePressed(showhide)
 }
 
 function draw() {
   background(125);
+
+  if (drawText) text("here's the text", 10,50);
+
+}
+
+function showhide() {
+  drawText = !drawText // set it to true if it was false, and vice versa
 }
